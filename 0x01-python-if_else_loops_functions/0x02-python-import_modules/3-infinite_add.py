4cd4f3c73101298d7759d76304a803d3c8bb911d
Steve <evilco30@gmail.com>
2023-03-09 14:15:49 -0800

    Python Modules

---
+++ 0x01-python-if_else_loops_functions/0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+
+sum = 0
+itr = len(sys.argv)
+
+if itr == 1:
+     print("{:d}".format(0))
+if itr == 2:
+    print("{:d}".format(int(sys.argv[1])))
+
+if itr > 2:
+    for i in range(1, itr):
+        sum += int(sys.argv[i])
+    print("{:d}".format(sum))
