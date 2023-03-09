94b2527ab0822d7375bcaa80c5d2f895702b4562
Steve <evilco30@gmail.com>
2023-03-09 14:32:13 -0800

    New

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 import sys
 
-itr = len(sys.argv)
+if __name__ == "__main__":
 
-if itr == 1:
-    print("0 arguments.")
+    itr = len(sys.argv)
 
-if itr == 2:
-    print("1 argument:")
-    print("{:d}: {:s}".format(1, sys.argv[1]))
+    if itr == 1:
+        print("0 arguments.")
 
-if itr > 2:
-    print("{:d} arguments:".format(itr - 1))
-    for i in range(1, itr):
-        print("{:d}: {:s}".format(i, sys.argv[i]))
+    if itr == 2:
+        print("1 argument:")
+        print("{:d}: {:s}".format(1, sys.argv[1]))
+
+    if itr > 2:
+        print("{:d} arguments:".format(itr - 1))
+        for i in range(1, itr):
+            print("{:d}: {:s}".format(i, sys.argv[i]))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 import sys
+if __name__ == "__main__":
+    sum = 0
+    itr = len(sys.argv)
 
-sum = 0
-itr = len(sys.argv)
+    if itr == 1:
+        print("{:d}".format(0))
+    if itr == 2:
+        print("{:d}".format(int(sys.argv[1])))
 
-if itr == 1:
-     print("{:d}".format(0))
-if itr == 2:
-    print("{:d}".format(int(sys.argv[1])))
-
-if itr > 2:
-    for i in range(1, itr):
-        sum += int(sys.argv[i])
-    print("{:d}".format(sum))
+    if itr > 2:
+        for i in range(1, itr):
+            sum += int(sys.argv[i])
+        print("{:d}".format(sum))
