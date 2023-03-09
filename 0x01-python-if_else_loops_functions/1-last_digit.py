04ea4c2b3779de0322cb991d56ee1f017c591ec0
Steve <evilco30@gmail.com>
2023-03-09 09:50:06 -0800

    Task 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,11 +7,11 @@ lastnum = int(repr(number)[-1])
 
 def check(lastnum):
     if lastnum > 5:
-        return("and is greater than 5\n")
+        return("and is greater than 5")
     if lastnum == 0:
-        return("and is 0\n")
+        return("and is 0")
     else:
-        return("and is less than 6 and not 0\n")
+        return("and is less than 6 and not 0")
 
 
 print("Last digit of {} is {} {}".format(number, lastnum, check(lastnum)))
