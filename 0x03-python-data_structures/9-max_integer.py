81cbf437197ce6b1b447da4cd8ed738242110031
Steve <evilco30@gmail.com>
2023-03-11 03:11:21 -0800

    Uploads

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,11 +2,11 @@
 
 
 def max_integer(my_list=[]):
- 
+
     lengthl = len(my_list)
     if lengthl == 0:
         return None
-    
+
     else:
         max_number = my_list[0]
         numbers = my_list[1:]
@@ -14,5 +14,5 @@ def max_integer(my_list=[]):
         for i in numbers:
             if i > max_number:
                 max_number = i
-        
+
         return max_number
