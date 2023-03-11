4b459d1b0f09496d2994a37a90e1cd8bd5c1de20
Steve <evilco30@gmail.com>
2023-03-11 03:22:46 -0800

    PEP

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -8,13 +8,13 @@ def add_tuple(tuple_a=(), tuple_b=()):
     # Check if tupples are empty
     if tup_a < 1:
         tuple_a = (0, 0)
-    elif tup_b < 1:
+    if tup_b < 1:
         tuple_b = (0, 0)
 
     # Check if tupples has only one element
     if tup_a < 2:
         tuple_a = (tuple_a[0], 0)
-    elif tup_b < 2:
+    if tup_b < 2:
         tuple_b = (tuple_b[0], 0)
 
     new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
