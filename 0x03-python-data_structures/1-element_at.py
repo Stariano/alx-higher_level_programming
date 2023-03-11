63e49a52320c70fed421a0215332f34cc8d1e30a
Steve <evilco30@gmail.com>
2023-03-11 03:31:06 -0800

    Mm

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,6 +2,6 @@
 
 def element_at(my_list, idx):
     checker = len(my_list)
-    if idx < 0 or idx > checker:
-        return
-    print("{}".format(my_list[idx]))
+    if idx < 0 or idx > checker - 1:
+        return None
+    return my_list[idx]
--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,7 +3,7 @@
 
 def replace_in_list(my_list, idx, element):
     checker = len(my_list)
-    if idx < 0 or idx > checker:
+    if idx < 0 or idx > checker - 1:
         return my_list
     my_list[idx] = element
     return (my_list)
