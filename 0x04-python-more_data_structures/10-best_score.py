1baea65e116ec7081bdbe392b8c1a309788f14c5
Steve <evilco30@gmail.com>
2023-03-20 16:09:28 -0700

    Task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,13 +3,11 @@
 
 def best_score(a_dictionary):
     max = 0
-    key = 0
     if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
 
     for i in a_dictionary.values():
         if i > max:
             max = i
-            key += 1
-    highest = list(a_dictionary)[key - 1]
-    return a_dictionary[key]
+    highest = [i for i in a_dictionary if a_dictionary[i]== max][0]
+    return highest
