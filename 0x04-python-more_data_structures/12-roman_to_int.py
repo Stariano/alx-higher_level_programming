6b221ee4db025f72f34b7f55ddb4ffc2a7b60dca
Steve <evilco30@gmail.com>
2023-03-20 16:20:03 -0700

    12 Hot fix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,21 +3,21 @@
 
 def roman_to_int(roman_string):
     num = 0
-    if isinstance(roman_string, str):
-        for i in roman_string:
-            if i == 'I':
-                num += 1
-            elif i == 'V':
-                num += 5
-            elif i == 'X':
-                num += 10
-            elif i == 'L':
-                num += 50
-            elif i == 'C':
-                num += 100
-            elif i == 'D':
-                num += 500
-            elif i == 'M':
-                num += 1000
-        return num
-    return 0
+    if not roman_string or type(roman_string) != str:
+        return 0
+    for i in roman_string:
+        if i == 'I':
+            num += 1
+        elif i == 'V':
+            num += 5
+        elif i == 'X':
+            num += 10
+        elif i == 'L':
+            num += 50
+        elif i == 'C':
+            num += 100
+        elif i == 'D':
+            num += 500
+        elif i == 'M':
+            num += 1000
+    return num
