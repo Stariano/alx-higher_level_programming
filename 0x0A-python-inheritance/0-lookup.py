93277a13d446e3c2338d9332cd786bd9adbbb157
Steve <evilco30@gmail.com>
2023-04-11 15:36:42 -0700

    All

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""First task provided"""
+
+
+def lookup(obj):
+    """ a function that returns the list of available attributes"""
+    return (dir(obj))
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,1 @@
+This repo will contain all of the applications on the inheritance task provided by ALX.
