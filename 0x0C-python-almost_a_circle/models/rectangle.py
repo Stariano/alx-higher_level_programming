ba115fb580e736643293b67c8f678c327db6204e
Starian <Starian>
2023-04-17 08:32:04 -0700

    All Push

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -31,6 +31,7 @@ class Rectangle(Base):
     def width(self):
         """Return the value of width"""
         return self.__width
+
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
@@ -43,9 +44,10 @@ class Rectangle(Base):
     def height(self):
         """Return the value of height"""
         return self.__height
+
     @height.setter
     def height(self, value):
-         if not isinstance(value, int):
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
@@ -83,10 +85,70 @@ class Rectangle(Base):
 
     def display(self):
         """ prints in stdout the Rectangle instance with the character """
-        for i in self.height:
-            for wid in self.width:
+        if self.width == 0 or self.height == 0:
+            print("")
+            return
+        for line in range(self.y):
+            print("")
+        for lenn in range(self.height):
+            for space in range(self.x):
+                print(" ", end="")
+            for w in range(self.width):
                 print("#", end="")
             print()
+
+    def update(self, *args, **kwargs):
+        """Update the Rectangle.
+        Args:
+            *args (ints): New attribute values.
+                - 1st argument represents id attribute
+                - 2nd argument represents width attribute
+                - 3rd argument represent height attribute
+                - 4th argument represents x attribute
+                - 5th argument represents y attribute
+            **kwargs (dict): New key/value pairs of attributes.
+        """
+        if args and len(args) != 0:
+            a = 0
+            for arg in args:
+                if a == 0:
+                    if arg is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif a == 1:
+                    self.width = arg
+                elif a == 2:
+                    self.height = arg
+                elif a == 3:
+                    self.x = arg
+                elif a == 4:
+                    self.y = arg
+                a += 1
+        elif kwargs and len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "id":
+                    if v is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = v
+                elif k == "width":
+                    self.width = v
+                elif k == "height":
+                    self.height = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
+
+    def to_dictionary(self):
+        """Return the dictionary representation."""
+        return {
+            "id": self.id,
+            "size": self.width,
+            "x": self.x,
+            "y": self.y
+        }
 
     def __str__(self):
         """Override"""
