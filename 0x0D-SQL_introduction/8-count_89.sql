b01dfb6f5f224908376e3a9050dfe7f983582ae7
Steve <evilco30@gmail.com>
2023-05-17 08:14:40 -0700

    The same error

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,3 +1,5 @@
 -- A script that displays the number of records with id = 89 in the table first_table.
 
-SELECT COUNT(*) FROM `first_table` WHERE `id` = 98;
+SELECT COUNT(*)
+FROM `first_table`
+WHERE `id` = 89;
