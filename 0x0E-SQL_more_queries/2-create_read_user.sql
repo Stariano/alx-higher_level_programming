a646d05b4e699dc58f5b7cd3268f75ea5b15371d
Steve <evilco30@gmail.com>
2023-05-19 13:15:34 -0700

    Task 3 check

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -7,4 +7,4 @@ CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
 
 -- Grant select
-GRANT SELECT ON 'hbtn_0d_2'.* TO 'hbtn_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'hbtn_0d_2'@'localhost';
