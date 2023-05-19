0f5301be578e2c261e811612840268c1336bcb2f
Steve <evilco30@gmail.com>
2023-05-19 13:52:41 -0700

    Adding UNIQUE constraint

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -7,7 +7,7 @@ USE hbtn_0d_usa;
 
 -- Create the table.
 CREATE TABLE IF NOT EXISTS states(
-        id INT NOT NULL AUTO_INCREMENT,
+        id INT UNIQUE NOT NULL AUTO_INCREMENT,
         name VARCHAR(256) NOT NULL,
 	PRIMARY KEY (id)
         );
