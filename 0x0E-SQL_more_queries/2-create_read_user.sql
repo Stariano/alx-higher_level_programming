a6c24024448825d34db1f886ab7aaf5355cf6e3f
Steve <evilco30@gmail.com>
2023-05-19 13:17:07 -0700

    Pushh

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,10 +1,10 @@
 -- A script that creates the database hbtn_0d_2.
 
--- Create a user
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-
 -- Create a database
 CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
 
+-- Create a user
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
 -- Grant select
-GRANT SELECT ON hbtn_0d_2.* TO 'hbtn_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
