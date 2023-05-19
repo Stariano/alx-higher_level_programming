67cfac11b886d0577082e89c60a2348f962de81f
Steve <evilco30@gmail.com>
2023-05-19 13:54:00 -0700

    Update

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,5 @@
 -- A script that creates the database hbtn_0d_usa and table states.
-
-CREATE DATABASE IN NOT EXISTS hbtn_0d_usa;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
 -- Use the db.
 USE hbtn_0d_usa;
