b2603318a45aa164adfff8052cbad654b0fc4ace
Steve <Steve>
2023-07-08 11:19:51 -0700

    Fixed

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # bash script for posting json data files and testing a server
-curl -sX POST "$1" -d "$2"
+curl -sX POST "$1" -d "@$2" -H "Content-Type: application/json"
--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that was a fun effort in breaking to http protocols on holberton serve
-curl -s -o /dev/null -w "%{body_response}" "$1"
+curl -sX PUT -H "origin:School" -d "user_id=98" 0.0.0.0:5000/catch_me_3
