5e27f61260a8b1e23c696b5fdb1209257aa0284f
Steve <evilco30@gmail.com>
2023-07-08 09:36:18 -0700

    Pushing

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" a function that finds a peak in a list of unsorted integers. """
+
+def find_peak(list_of_integers):
+    """BRUTE force implementation for question
+    """
+    max_i = None
+    for ele in list_of_integers:
+        if max_i is None or max_i < ele:
+            max_i = ele
+    return max_i
